3b7aa4d8e842e87cb58cb769d2aa518d163315ef
Phillip Andersen <phillip_andersen@hotmail.com>
2020-09-11 14:52:56 +0200

    done

--- src/test/java/rest/AllEndPointsTest.java
+++ src/test/java/rest/AllEndPointsTest.java
@@ -14,19 +14,23 @@ import org.glassfish.grizzly.http.util.HttpStatus;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 //Uncomment the line below, to temporarily disable this test
-@Disabled
+//@Disabled
+
 public class AllEndPointsTest {
 
     private static final int SERVER_PORT = 7777;
     private static final String SERVER_URL = "http://localhost/api";
-    private static Movie m1,m2;
-    
+    private static Movie m1, m2;
+
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
     private static HttpServer httpServer;
     private static EntityManagerFactory emf;
@@ -41,73 +45,102 @@ public class AllEndPointsTest {
         //This method must be called before you request the EntityManagerFactory
         EMF_Creator.startREST_TestWithDB();
         emf = EMF_Creator.createEntityManagerFactoryForTest();
-        
+
         httpServer = startServer();
         //Setup RestAssured
         RestAssured.baseURI = SERVER_URL;
         RestAssured.port = SERVER_PORT;
         RestAssured.defaultParser = Parser.JSON;
     }
-    
+
     @AfterAll
-    public static void closeTestServer(){
+    public static void closeTestServer() {
         //System.in.read();
-         //Don't forget this, if you called its counterpart in @BeforeAll
-         EMF_Creator.endREST_TestWithDB();
-         httpServer.shutdownNow();
+        //Don't forget this, if you called its counterpart in @BeforeAll
+        EMF_Creator.endREST_TestWithDB();
+        httpServer.shutdownNow();
     }
-    
+
     // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
     //TODO -- Make sure to change the EntityClass used below to use YOUR OWN (renamed) Entity class
     @BeforeEach
     public void setUp() {
         EntityManager em = emf.createEntityManager();
-        m1 = new Movie("Test2", "Test2", "Test2", 1995);
-        m2 = new Movie("Test1", "Test1", "Test1", 1995);
+        m1 = new Movie("Test2", "Test2", "Test2", 2000);
+        m2 = new Movie("Test1", "Hej", "Med", 1995);
         try {
             em.getTransaction().begin();
-          em.createNativeQuery("DELETE FROM MOVIE").executeUpdate();
+            em.createNativeQuery("DELETE FROM MOVIE").executeUpdate();
+            em.createNativeQuery("alter table MOVIE AUTO_INCREMENT = 1").executeUpdate();
             em.persist(m1);
-            em.persist(m2); 
+            em.persist(m2);
             em.getTransaction().commit();
-        } finally { 
+        } finally {
             em.close();
         }
     }
-    
+
     @Test
     public void testServerIsUp() {
         System.out.println("Testing is server UP");
-        given().when().get("/xxx").then().statusCode(200);
+        given()
+                .when()
+                .get("/xxx")
+                .then()
+                .statusCode(200);
+
     }
-   
+
     //This test assumes the database contains two rows
     @Test
     public void testDummyMsg() throws Exception {
         given()
-        .contentType("application/json")
-        .get("/xxx/").then()
-        .assertThat()
-        .statusCode(HttpStatus.OK_200.getStatusCode())
-        .body("msg", equalTo("Hello World"));   
+                .contentType("application/json")
+                .get("/xxx/").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("msg", equalTo("Hello World"));
     }
-    
-    @Test
+
+   /* @Test
     public void testCount() throws Exception {
         given()
-        .contentType("application/json")
-        .get("/xxx/getMovieCount").then()
-        .assertThat()
-        .statusCode(HttpStatus.OK_200.getStatusCode())
-        .body("getMovieCount", equalTo(1));   
+                .contentType("application/json")
+                .get("/xxx/getMovieCount").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("getMovieCount", is(2));
+    }*/
+
+    @Test
+    public void testAllMovies() throws Exception {
+        given()
+                .contentType("application/json")
+                .get("/xxx/getMovieList").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("size()", is(2)).and().body("title", hasItems("Test1", "Test2"));
+
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    @Test
+    public void testSpecificTitle() throws Exception {
+        given()
+                .contentType("application/json")
+                .get("/xxx/title/Test1").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("title", equalTo(m2.getTitle()));
+    }
+
+    /*@Test
+    public void testSpecificYear() throws Exception {
+        given()
+                .contentType("application/json")
+                .get("/xxx/year/2000").then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode())
+                .body("year", equalTo(m1.getReleaseYear()));
+    }*/
+
 }
